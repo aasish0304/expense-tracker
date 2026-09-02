8fd22581653ffc836f954d4b4ed1ac2fae4e3fce
Aasish Korukonda <aasishash03@gmail.com>
2026-09-02 15:02:31 +0530

    test(ci): add automated backend tests

--- backend/expenses/tests.py
+++ backend/expenses/tests.py
@@ -1,3 +1,13 @@
 from django.test import TestCase
+from django.urls import reverse
 
-# Create your tests here.
+
+class BackendSmokeTests(TestCase):
+
+    def test_api_token_url_exists(self):
+        response = self.client.get("/api/token/")
+        self.assertNotEqual(response.status_code, 404)
+
+    def test_expenses_api_url_exists(self):
+        response = self.client.get("/api/expenses/")
+        self.assertNotEqual(response.status_code, 404)
