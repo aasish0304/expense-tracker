f04df317eff246e1742853839697206e17fa9c37
Aasish Korukonda <aasishash03@gmail.com>
2026-09-07 11:59:14 +0530

    feat(backend): connect to PostgreSQL RDS

--- backend/expense_tracker/settings.py
+++ backend/expense_tracker/settings.py
@@ -104,12 +104,24 @@ TEMPLATES = [
 # DATABASE
 # =====================================================
 
-DATABASES = {
-    "default": {
-        "ENGINE": "django.db.backends.sqlite3",
-        "NAME": BASE_DIR / "db.sqlite3",
+if os.getenv("DB_HOST"):
+    DATABASES = {
+        "default": {
+            "ENGINE": "django.db.backends.postgresql",
+            "NAME": os.getenv("DB_NAME"),
+            "USER": os.getenv("DB_USER"),
+            "PASSWORD": os.getenv("DB_PASSWORD"),
+            "HOST": os.getenv("DB_HOST"),
+            "PORT": os.getenv("DB_PORT", "5432"),
+        }
     }
-}
+else:
+    DATABASES = {
+        "default": {
+            "ENGINE": "django.db.backends.sqlite3",
+            "NAME": BASE_DIR / "db.sqlite3",
+        }
+    }
 
 
 # =====================================================
